7cf7ad41e21a16b768e90df8387eb4202e3cdb89
LeonardoGabbrielli <leonardo.gabbrielli@capellinisauro.edu.it>
2026-05-25 11:23:18 +0000

    Stampa compleanno

--- helloworld.java
+++ helloworld.java
@@ -5,5 +5,6 @@ public class helloworld{
         System.out.println("Hello Gabbrielli"); 
         System.out.println("Hello Gabbrielli"); 
         System.out.println("Hello Gabbrielli");
+        System.out.println("19/04/2008");
     }
 }
